fb62766806fac712849cc2f5770deffd4a5698aa
Lungu Olga <lunguolga2000@yahoo.com>
2020-06-01 22:42:16 +0300

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Product;
+
+class HomeController extends Controller
+{
+   public function index(){
+       $products=Product::orderBy('created_at')->take(8)->get();
+
+      return view('home.index',[
+          'products'=>$products
+      ]);
+
+   }
+}
---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/','HomeController@index');
+Route::get('/{cat}', 'ProductController@showCategory')->name('showCategory');
+Route::get('/{cat}/{product_id}', 'ProductController@show')->name('showProduct');
